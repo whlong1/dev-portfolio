2a8f967624f51cf656c48fa5ac8a1eef493b68f1
whlong1 <whunterlong@gmail.com>
2023-06-13 01:10:54 -0400

    Build out state for contact form, add handle change and handlesubmit functions

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,31 +1,92 @@
+import { useState } from 'react'
+
+interface FormData {
+  firstName: string,
+  lastName: string,
+  email: string,
+  message: string,
+}
+
+const initialState = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  message: '',
+}
+
 const ContactForm = () => {
+  const [formData, setFormData] = useState<FormData>(initialState)
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [target.name]: target.value })
+  }
+
+  const handleSubmit = async (evt: React.FormEvent): Promise<void> => {
+    evt.preventDefault()
+    try {
+      console.log('Data', formData)
+      setFormData(initialState)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <div className="form-row">
         <div className="form-column">
-          <label htmlFor="firstName">FIRST NAME</label>
-          <input type="text" id="firstName" name="firstName" />
+          <label htmlFor="firstName">
+            FIRST NAME
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            onChange={handleChange}
+            value={formData.firstName}
+          />
         </div>
         <div className="form-column">
-          <label htmlFor="lastName">LAST NAME</label>
-          <input type="text" id="lastName" name="lastName" />
+          <label htmlFor="lastName">
+            LAST NAME
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            onChange={handleChange}
+            value={formData.lastName}
+          />
         </div>
       </div>
-
       <div className="form-row">
         <div className="form-column">
-          <label htmlFor="email">EMAIL</label>
-          <input type="text" id="email" name="email" />
+          <label htmlFor="email">
+            EMAIL
+          </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={handleChange}
+            value={formData.email}
+          />
         </div>
       </div>
-
       <div className="form-row">
         <div className="form-column">
-          <label htmlFor="message">MESSAGE</label>
-          <input type="text" id="message" name="message" />
+          <label htmlFor="message">
+            MESSAGE
+          </label>
+          <input
+            type="text"
+            id="message"
+            name="message"
+            onChange={handleChange}
+            value={formData.message}
+          />
         </div>
       </div>
-
       <button type="submit">SEND</button>
     </form>
   )
