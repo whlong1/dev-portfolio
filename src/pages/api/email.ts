45a5fedbc8d9d34f1f5bf179e14da96650496c0d
whlong1 <whunterlong@gmail.com>
2023-06-14 16:01:48 -0400

    Add markdown for confirmation message

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,9 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import nodemailer from "nodemailer"
 
-interface ErrorResponse { error: string; }
-interface SuccessResponse { msg: string; }
+interface Message {
+  cc: string;
+  to: string;
+  html: string;
+  text: string;
+  from: string;
+  subject: string;
+}
 
+interface ErrorResponse { error: string; }
+interface SuccessResponse { msg: string; sender: string }
 type Res = ErrorResponse | SuccessResponse
 
 const transporter = nodemailer.createTransport({
@@ -22,26 +30,43 @@ const sendEmail = async (
 ): Promise<void> => {
 
   const formattedContent = `
-    <h1>Thank you for your message!</h1>
-    <p>${req.body.message}</p>
+    <h1>Confirmation</h1>
+    <p>Thanks for reaching out. I'll get back to you as soon as possible!</p>
+
+    <p>
+      <b>Below is a copy of your message for reference:</b>
+    </p>
+
+    <blockquote style="margin-left: 0; padding: 10px 20px; border-radius: 5px; background-color: #e8e8e8;">
+      ${req.body.message}
+    </blockquote>
+
+    <p style="margin-bottom: 8px">Best regards,</p>
+    <p style="margin-top: 8px">Hunter Long</p>
   `
-  
-  const message = {
+
+  const message: Message = {
     cc: req.body.email,
     html: formattedContent,
     text: req.body.message,
-    to: process.env.EMAIL_ADDRESS,
-    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS!,
+    from: process.env.EMAIL_ADDRESS!,
     subject: `Message from ${req.body.firstName} ${req.body.lastName}`,
   }
 
-  if (req.method === 'POST') {
-    console.log('Form data:', req.body)
-    const sendStatus = await transporter.sendMail(message)
-    console.log(sendStatus)
-    res.status(200).json({ msg: 'Success' })
+  if (req.method === "POST") {
+    try {
+      await transporter.sendMail(message)
+      res.status(200).json({
+        sender: req.body.email,
+        msg: "Your message has been sent. Please check the email below for confirmation.",
+      })
+    } catch (err) {
+      console.error("Send Mail Error:", err)
+      res.status(500).json({ error: "An error occurred while sending your message." })
+    }
   } else {
-    res.status(405).json({ error: `Invalid Method` })
+    res.status(405).json({ error: "Invalid Method" })
   }
 }
 
