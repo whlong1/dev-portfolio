e6262a8cd65b19080f953cd6484f7bf72dc3b13c
whlong1 <whunterlong@gmail.com>
2023-07-11 12:25:00 -0400

    Add initial transitions for slideout menu, add wrapper element

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -3,13 +3,14 @@ import NavLinks from "./NavLinks"
 
 const HamburgerMenu = () => {
   const [open, setOpen] = useState(true)
+  const toggleStyle = `${open ? 'open' : ''}`
   return (
     <>
-      {open &&
-        <div className="menu-wrapper">
+      <div className={`menu-wrapper ${toggleStyle}`}>
+        <div className={`menu ${toggleStyle}`}>
           <NavLinks />
         </div>
-      }
+      </div>
       <button onClick={() => setOpen((prev) => !prev)}>
         <img src="assets/icons/hamburger.svg" alt="Open Menu" />
       </button>
