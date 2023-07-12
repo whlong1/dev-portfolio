ae026868ab8d156d28a439717735c4520278ec7c
whlong1 <whunterlong@gmail.com>
2023-07-11 22:14:22 -0400

    Add updated social icons, fix file paths

--- src/components/DetailContainer.tsx
+++ src/components/DetailContainer.tsx
@@ -13,7 +13,7 @@ const DetailContainer = ({ project }: DetailContainerProps) => {
         <a href={project.github} target="_blank" rel="noopener noreferrer">
           <img
             className="card-header-icon"
-            src="/assets/icons/github.svg"
+            src="/assets/social/github.svg"
             alt="GitHub Logo"
           />
         </a>
