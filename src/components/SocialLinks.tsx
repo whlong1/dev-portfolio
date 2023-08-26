165aaf4b40b66c94edfaf545eba20f9fad80669e
whlong1 <whunterlong@gmail.com>
2023-08-26 16:39:41 -0400

    Update social links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -2,19 +2,19 @@ const SocialLinks = () => {
   return (
     <ul className="social-links">
       <li>
-        <a href="">
+        <a href="https://www.linkedin.com/in/william-hunter-long/">
           <img src="/assets/social/linkedin.svg" alt="LinkedIn" />
           <p>LinkedIn</p>
         </a>
       </li>
       <li>
-        <a href="">
+        <a href="https://github.com/whlong1">
           <img src="/assets/social/github.svg" alt="GitHub" />
           <p>GitHub</p>
         </a>
       </li>
       <li>
-        <a href="">
+        <a href="mailto:hunterlong@zohomail.com">
           <img src="/assets/social/email.svg" alt="Email" />
           <p>Email</p>
         </a>
