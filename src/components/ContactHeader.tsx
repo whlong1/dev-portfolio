99c414bb1569f56bad830b54aba2a99795d0ded5
whlong1 <whunterlong@gmail.com>
2023-08-28 15:23:36 -0400

    Update copy

--- src/components/ContactHeader.tsx
+++ src/components/ContactHeader.tsx
@@ -5,7 +5,7 @@ const ContactHeader = () => {
     <header>
       <h2>CONTACT</h2>
       <p className="section-blurb">
-        Want to connect? Feel free to contact me through LinkedIn, GitHub, or email. For your convenience, you can also use this form to send a message. I can't wait to hear from you.
+        Want to connect? Feel free to contact me through LinkedIn, GitHub, or email. For your convenience, you can also use this form to send a message. Looking forward to hearing from you!
       </p>
       <SocialLinks />
     </header>
