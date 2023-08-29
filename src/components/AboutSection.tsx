0b3a92064ace1741b959f4d1447bfae6286446d5
whlong1 <whunterlong@gmail.com>
2023-08-28 21:00:57 -0400

    Update resume, update about blurb

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -8,13 +8,13 @@ const AboutSection = () => {
         <h6>FULL-STACK DEVELOPER</h6>
         <h1>HUNTER LONG</h1>
         <p>
-          As a full-stack software engineer with an extensive background in design, I bring a unique blend of creativity and technical expertise to the table. In my role as a Senior Instructional Associate at General Assembly, I teach and mentor aspiring developers. My focus is on creating digital products that balance aesthetic appeal with user-centric functionality.
+          As a full-stack software engineer with an extensive background in design, I bring a unique blend of creativity and technical expertise to the table. In my role as a Senior Instructional Associate at General Assembly, I develop new content in the software engineering curriculum and provide mentorship to aspiring developers. My focus is on crafting innovative digital products that balance aesthetic appeal with user-centric functionality.
         </p>
         <div className="cta-container">
           <a className="button" href="#contact">
             GET IN TOUCH
           </a>
-          <a className="button" href="/assets/documents/Hunter_Long_Resume_v7.pdf" target="_blank" rel="noopener noreferrer">
+          <a className="button" href="/assets/documents/hunterlong-resume-v8.pdf" target="_blank" rel="noopener noreferrer">
             RESUME
           </a>
         </div>
